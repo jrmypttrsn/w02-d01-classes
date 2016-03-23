0d92e7daa5c5c65fcc05a21332182fbfd2630ea9
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-23 16:56:47 -0500

    Fixed error in dog

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,22 +2,26 @@ require "pry"
 
 class Dog
 
-	attr_accessor :name :age :species :gender :tail :eyes :ears :license :shots? :rabbits_eaten
+	attr_accessor :name, :age, :species, :gender, :tail, :eyes, :ears, :license, :shots, :rabbits_killed
 
-	def initialize(options={})
-    @name = options[:name]
-    @age = options[:age]
-    @species = options[:species]
-    @gender = options[:gender]
-    @tail = options[:tail] 
-    @legs = options[:legs]
-    @eyes = options[:legs]
-    @ears = options[:ears]
-    @license = options[:license]
-    @shots? = options[:shots]
-    @rabbits_eaten = options[:rabbits_eaten]
+	def initialize(name:, age:, species:, gender:, tail:, legs:, eyes:, ears:, license:, 
+									shots:, rabbits_killed:)
+    @name = name
+    @age = age
+    @species = species
+    @gender = gender
+    @tail = tail
+    @legs = legs
+    @eyes = eyes
+    @ears = ears
+    @license = license
+    @shots = shots
+    @rabbits_killed = rabbits_killed
   end
 
+  def shots?
+  	@shots
+  end
 end
 
 #binding.pry
