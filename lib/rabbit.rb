ad0b59de734b4f937e055264b4bf3ec95f91a18c
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-22 21:37:16 -0500

    Refactored rabbit using attr accessor

--- lib/rabbit.rb
+++ lib/rabbit.rb
@@ -2,13 +2,12 @@ require "pry"
 
 class Rabbit
 
-  attr_accessor :name :age :species :tail :legs :eyes :ears :children
+  attr_accessor :name :age :species :gender :legs :eyes :ears :children
 
 	def initialize(options={})
     @name = options[:name]
     @age = options[:age]
     @species = options[:species]
-    @tail = options[:tail] 
     @legs = options[:legs]
     @eyes = options[:legs]
     @ears = options[:ears]
