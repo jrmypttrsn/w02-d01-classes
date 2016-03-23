fecb5e1cd9234ccd058ba93c8a7b37588d13ed58
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-22 21:58:44 -0500

    Some change was made to dog

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,5 @@
 require "pry"
 
-# What is the name of the dog?
-# What is the age of the dog?
-# What is the species of the dog?
-# What is the gender of the dog?
-# Does the dog have a tail?
-# How many legs does the dog have?
-# How many eyes does the dog have?
-# How many ears does the dog have?
-# What is the dog's license number?
-# Is the dog up to date on their shots?
-
-
 class Dog
 
 	attr_accessor :name :age :species :gender :tail :eyes :ears :license :shots
