2b5c45b40f4d0c71d64f930a9f31213128716ffa
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-22 21:19:37 -0500

    Refactored dog to using attr accessor

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,6 +14,8 @@ require "pry"
 
 class Dog
 
+	attr_accessor :name :age :species :gender :tail :eyes :ears :license :shots
+
 	def initialize(options={})
     @name = options[:name]
     @age = options[:age]
@@ -25,86 +27,6 @@ class Dog
     @ears = options[:ears]
     @license = options[:license]
     @shots = options[:shots]
-  end
-
-  def name
-    @name
-  end
-
-  def name=(new_name)
-    @name = new_name
-  end
-
-  def age
-    @age
-  end
-
-  def age=(new_age)
-    @age = new_age if new_age >= 1
-  end
-
-  def species
-  	@species
-  end
-
-  def species=(new_species)
-  	@species
-  end
-
-  def gender
-  	@gender
-  end
-
-  def gender=(new_gender)
-  	@gender = new_gender
-  end
-
-  def tail
-    @tail
-  end
-
-  def tail=(new_tail)
-    @tail = new_tai
-  end
-
-  def legs
-    @legs
-  end
-
-  def legs=(new_legs)
-    @legs = new_legs
-  end
-
-  def eyes
-    @eyes
-  end
-
-  def eyes=(new_eyes)
-    @eyes = new_eyes
-  end
-
-  def ears
-  	@ears
-  end
-
-  def ears=(new_ears)
-  	@ears
-  end
-
-  def license
-  	@license
-  end
-
-  def license=(new_license)
-  	@license
-  end
-
-  def shots
-  	@shots
-  end
-
-  def shots=(new_shots)
-  	@shots
   end
 
 end
