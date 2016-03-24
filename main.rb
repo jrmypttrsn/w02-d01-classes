904ca04ba506e9e24b04ba4fe7f22e42c1220bc8
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-23 22:38:02 -0500

    Made changes to main

--- main.rb
+++ main.rb
@@ -4,8 +4,8 @@ require_relative "lib/rabbit.rb"
 
 def create_a_dog
   puts "\n\nCREATE A DOG OUT OF THIN AIR!\u2122\n"
-  puts "More fun than a toothache=E2=84=A2!"
-  print "What would you like your dog to be called? "
+  puts "More fun than a toothache\u00A9!"
+  print "\nWhat would you like your dog to be called? "
   name = gets.chomp
 
   print "How old is #{name}? "
@@ -38,7 +38,37 @@ def create_a_dog
 
   Dog.new(name: name, age: age, species: species, gender: gender, tail: tail, 
     legs: legs, eyes:eyes, ears: ears, license: license, rabbits_killed: rabbits_killed)
+end
 
+def create_a_rabbit
+  puts "\n\nCREATE A RABBIT BY TYPING THINGS!\u2122\n"
+  puts "You might get carpal tunnel syndrome!\u00A9"
+  print "\nWhat's a good name for a rabbit? "
+  name = gets.chomp
+
+  print "How old is #{name}? "
+  age = gets.chomp.to_i
+
+  print "What species is #{name}? "
+  species = gets.chomp
+
+  print "What gender is #{name}? "
+  gender = gets.chomp
+  
+  print "How many legs does #{name} have? "
+  legs = gets.chomp.to_i
+
+  print "How many eyes does #{name} have? "
+  eyes = gets.chomp.to_i
+
+  print "How many ears does #{name} have? "
+  ears = gets.chomp.to_i
+
+  print "How many children does #{name} have? "
+  children = gets.chomp.to_i
+
+  Rabbit.new(name: name, age: age, species: species, gender: gender, legs: legs, 
+    eyes:eyes, ears: ears, children: children)
 end
 
 def dog_data(dog)
@@ -50,13 +80,12 @@ def dog_data(dog)
   puts "#{dog.name} has killed #{dog.rabbits_killed} rabbit(s)."
 end
 
-def dog_data(dog)
-  puts "\n\n\nDOG DETAILS\n"
-  puts "Your dog is named #{dog.name} and is #{dog.age} years old."
-  puts "#{dog.name} is a #{dog.gender} #{dog.species}."
-  puts "#{dog.name} has a #{dog.tail} tail, #{dog.legs} legs, #{dog.eyes} eyes and #{dog.ears} ears."
-  puts "#{dog.name}'s license number is #{dog.license}"
-  puts "#{dog.name} has killed #{dog.rabbits_killed} rabbit(s)."
+def rabbit_census(dog)
+  puts "\n\n\nRABBIT RECORD\n"
+  puts "Your rabbit is named #{rabbit.name} and is #{rabbit.age} years old."
+  puts "#{rabbit.name} is a #{rabbit.gender} #{rabbit.species}."
+  puts "#{rabbit.name} has #{rabbit.legs} legs, #{rabbit.eyes} eyes and #{rabbit.ears} ears."
+  puts "#{rabbit.name} has #{rabbit.children} children."
 end
 
 def report_all_dogs(arr)
@@ -65,12 +94,16 @@ def report_all_dogs(arr)
   end
 end
 
-
+def report_all_rabbits(arr)
+  arr.each do |rabbit|
+    rabbit_census(rabbit)
+  end
+end
 
 dogs = []
 rabbits = []
 
-choice = 3
+choice = 6
 
 puts "WELCOME TO THE BACKYARD WHERE ANYTHING GOES\u00AE"
 
