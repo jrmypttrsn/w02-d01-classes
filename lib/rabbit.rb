897ebfcec29d12dc00849d43b1396839a541e2f7
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-03-24 08:32:32 -0500

    Removed : from attributes in rabbit

--- lib/rabbit.rb
+++ lib/rabbit.rb
@@ -4,15 +4,15 @@ class Rabbit
 
   attr_accessor :name, :age, :species, :gender, :legs, :eyes, :ears, :children
 
-	def initialize(options={})
-    @name = options[:name]
-    @age = options[:age]
-    @species = options[:species]
-    @gender = options[:gender]
-    @legs = options[:legs]
-    @eyes = options[:eyes]
-    @ears = options[:ears]
-    @children = options[:children]
+	def initialize(name:, age:, species:, gender:, legs:, eyes:, ears:, children:)
+    @name = name
+    @age = age
+    @species = species
+    @gender = gender
+    @legs = legs
+    @eyes = eyes
+    @ears = ears
+    @children = children
   end
   
 end
